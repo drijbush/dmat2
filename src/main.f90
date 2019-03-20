8e65bfcd787046813295175e15a1192abc0098fc
me <my.business@not.yours>
2019-03-20 15:04:40 +0000

    Extendning testing of matmult

--- src/main.f90
+++ src/main.f90
@@ -12,7 +12,7 @@ Program test_distributed_matrix
 
   Integer :: me, nproc
   Integer :: n, m, k
-  Integer :: nb
+  Integer :: n_block
   Integer :: error
 
   Call mpi_init( error )
@@ -23,16 +23,17 @@ Program test_distributed_matrix
   If( me == 0 ) Then
      Write( *, * ) 'm, n, k ?'
      Read ( *, * )  m, n, k
-     Write( *, * ) 'nb ?'
-     Read ( *, * )  nb
+     Write( *, * ) 'n_block ?'
+     Read ( *, * )  n_block
   End If
   Call mpi_bcast( m , 1, mpi_integer, 0, mpi_comm_world, error )
   Call mpi_bcast( n , 1, mpi_integer, 0, mpi_comm_world, error )
   Call mpi_bcast( k , 1, mpi_integer, 0, mpi_comm_world, error )
-  Call mpi_bcast( nb, 1, mpi_integer, 0, mpi_comm_world, error )
+  Call mpi_bcast( n_block, 1, mpi_integer, 0, mpi_comm_world, error )
 
   Call test_real_matmul_NN
   Call test_real_matmul_TN
+  Call test_real_matmul_NT
   Call test_complex_matmul_NN
   
   Call mpi_finalize( error )
@@ -55,9 +56,22 @@ Contains
     Type( real_distributed_matrix ) :: base
     Class( distributed_matrix ), Allocatable :: Am, Bm, Cm
 
-    Allocate( A( 1:m, 1:k ) )
-    Allocate( B( 1:k, 1:n ) )
-    Allocate( C( 1:m, 1:n ) )
+    Integer :: ma, na
+    Integer :: mb, nb
+    Integer :: mc, nc
+
+    ma = m
+    na = k
+
+    mb = k
+    nb = n
+
+    mc = m
+    nc = n
+    
+    Allocate( A( 1:ma, 1:na ) )
+    Allocate( B( 1:mb, 1:nb ) )
+    Allocate( C( 1:mc, 1:nc ) )
     If( me == 0 ) Then
        Call Random_number( A )
        Call Random_number( B )
@@ -67,19 +81,19 @@ Contains
     Call mpi_bcast( B, Size( B ), mpi_double_precision, 0, mpi_comm_world, error )
     Call mpi_bcast( C, Size( C ), mpi_double_precision, 0, mpi_comm_world, error )
     
-    Allocate( tmp( 1:m, 1:n ) )
-
+    Allocate( tmp, Mold = C )
+    
     Call distributed_matrix_init
-    Call distributed_matrix_set_default_blocking( nb )
+    Call distributed_matrix_set_default_blocking( n_block )
     Call distributed_matrix_comm_to_base( mpi_comm_world, base )
     Allocate( real_distributed_matrix :: Am )
     Allocate( real_distributed_matrix :: Bm )
-    Call Am%create( m, k, base )
-    Call Am%set_by_global( 1, m, 1, k, A )
-    Call Bm%create( k, n, Am )
-    Call Bm%set_by_global( 1, k, 1, n, B )
+    Call Am%create( ma, na, base )
+    Call Am%set_by_global( 1, ma, 1, na, A )
+    Call Bm%create( mb, nb, Am )
+    Call Bm%set_by_global( 1, mb, 1, nb, B )
     Cm = Am * Bm
-    Call Cm%get_by_global( 1, m, 1, n, tmp )
+    Call Cm%get_by_global( 1, mc, 1, nc, tmp )
     If( me == 0 ) Then
        Write( *, * ) 'Error in real matmul NN ', Maxval( Abs( C - tmp ) )
     End If
@@ -118,7 +132,7 @@ Contains
     Allocate( tmp( 1:m, 1:n ) )
 
     Call distributed_matrix_init
-    Call distributed_matrix_set_default_blocking( nb )
+    Call distributed_matrix_set_default_blocking( n_block )
     Call distributed_matrix_comm_to_base( mpi_comm_world, base )
     Allocate( real_distributed_matrix :: Am )
     Allocate( real_distributed_matrix :: Bm )
@@ -130,14 +144,75 @@ Contains
     Cm  = AmT * Bm
     Call Cm%get_by_global( 1, m, 1, n, tmp )
     If( me == 0 ) Then
-       Write( *, * )  Am%size( 1 ),  Am%size( 2 )
-       Write( *, * ) AmT%size( 1 ), AmT%size( 2 )
-       Write( *, * )  Cm%size( 1 ),  Cm%size( 2 )
        Write( *, * ) 'Error in real matmul TN ', Maxval( Abs( C - tmp ) )
     End If
     Call distributed_matrix_finalise
 
   End Subroutine test_real_matmul_TN
+
+  Subroutine test_real_matmul_NT
+    
+    Use mpi, Only : mpi_bcast, mpi_comm_world, mpi_double_precision
+
+   Use numbers_module           , Only : wp
+    Use distributed_matrix_module, Only : distributed_matrix, real_distributed_matrix, &
+         distributed_matrix_init, distributed_matrix_comm_to_base, distributed_matrix_finalise, &
+         distributed_matrix_set_default_blocking
+
+    Implicit None
+    
+    Real( wp ), Dimension( :, : ), Allocatable :: A, B, C, tmp
+
+    Type( real_distributed_matrix ) :: base
+    Class( distributed_matrix ), Allocatable :: Am, Bm, BmT, Cm
+
+    Integer :: ma, na
+    Integer :: mb, nb
+    Integer :: mc, nc
+
+    ma = m
+    na = k
+
+    mb = n
+    nb = k
+
+    mc = m
+    nc = n
+    
+    Allocate( A( 1:m, 1:k ) )
+    Allocate( B( 1:n, 1:k ) )
+    Allocate( C( 1:m, 1:n ) )
+    If( me == 0 ) Then
+       Call Random_number( A )
+       Call Random_number( B )
+       C = Matmul( A, Transpose( B ) )
+       Write( *, * ) '!!!!!!', Shape( Matmul( A, Transpose( B ) ) )
+    End If
+    Call mpi_bcast( A, Size( A ), mpi_double_precision, 0, mpi_comm_world, error )
+    Call mpi_bcast( B, Size( B ), mpi_double_precision, 0, mpi_comm_world, error )
+    Call mpi_bcast( C, Size( C ), mpi_double_precision, 0, mpi_comm_world, error )
+
+    Allocate( tmp( 1:m, 1:n ) )
+    
+    Call distributed_matrix_init
+    Call distributed_matrix_set_default_blocking( n_block )
+    Call distributed_matrix_comm_to_base( mpi_comm_world, base )
+    Allocate( real_distributed_matrix :: Am )
+    Allocate( real_distributed_matrix :: Bm )
+    Call Am%create( m, k, base )
+    Call Am%set_by_global( 1, m, 1, k, A )
+    Call Bm%create( n, k, Am )
+    Call Bm%set_by_global( 1, n, 1, k, B )
+    BmT = .Dagger. Bm
+    Cm = Am * BmT
+    Write( *, * ) mc, nc, Cm%size( 1 ), Cm%size( 2 )
+    Call Cm%get_by_global( 1, m, 1, n, tmp )
+    If( me == 0 ) Then
+       Write( *, * ) 'Error in real matmul NT ', Maxval( Abs( C - tmp ) )
+    End If
+    Call distributed_matrix_finalise
+
+  End Subroutine test_real_matmul_NT
 
   Subroutine test_complex_matmul_NN
     
@@ -178,7 +253,7 @@ Contains
     Allocate( tmp( 1:m, 1:n ) )
 
     Call distributed_matrix_init
-    Call distributed_matrix_set_default_blocking( nb )
+    Call distributed_matrix_set_default_blocking( n_block )
     Call distributed_matrix_comm_to_base( mpi_comm_world, base )
     Allocate( complex_distributed_matrix :: Am )
     Allocate( complex_distributed_matrix :: Bm )
