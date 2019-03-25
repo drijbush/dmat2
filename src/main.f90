a5df096037a823c8c91d0aad78cd10573f2d322d
Ian Bush <Ian.Bush@oerc.ox.ac.uk>
2019-03-25 10:47:02 +0000

    Added first ks array test - NN matmul

--- src/main.f90
+++ src/main.f90
@@ -3,18 +3,18 @@ Program test_distributed_matrix
   Use numbers_module, Only : wp
   Use mpi, Only : mpi_init, mpi_finalize, mpi_comm_rank, mpi_comm_size, mpi_bcast, &
        mpi_comm_world, mpi_integer
-  Use ks_array_module, Only : ks_array
   
   Implicit None
 
   Integer :: me, nproc
   Integer :: n, m, k
   Integer :: n_block
+  Integer :: ns, nk
   Integer :: error
 
   Character( Len = 26 ), Parameter :: format = '( a, t35, g26.20, t65, a )'
 
-  Real( wp ), Parameter :: tol = 1.0e-12_wp
+  Real( wp ), Parameter :: tol = 1.0e-11_wp
   
   Call mpi_init( error )
 
@@ -26,11 +26,15 @@ Program test_distributed_matrix
      Read ( *, * )  m, n, k
      Write( *, * ) 'n_block ?'
      Read ( *, * )  n_block
+     Write( *, * ) 'ns, nk ?'
+     Read ( *, * )  ns, nk
   End If
   Call mpi_bcast( m , 1, mpi_integer, 0, mpi_comm_world, error )
   Call mpi_bcast( n , 1, mpi_integer, 0, mpi_comm_world, error )
   Call mpi_bcast( k , 1, mpi_integer, 0, mpi_comm_world, error )
   Call mpi_bcast( n_block, 1, mpi_integer, 0, mpi_comm_world, error )
+  Call mpi_bcast( ns, 1, mpi_integer, 0, mpi_comm_world, error )
+  Call mpi_bcast( nk, 1, mpi_integer, 0, mpi_comm_world, error )
 
   ! Distributed matrix tests
   Call test_real_matmul_NN
@@ -42,7 +46,7 @@ Program test_distributed_matrix
   Call test_complex_matmul_NT
   Call test_complex_matmul_TT
 
-  ! KSmatrix tests
+  ! KS_matrix tests
   Call test_ks_matrix_matmul_real_NN
   Call test_ks_matrix_matmul_real_TN
   Call test_ks_matrix_matmul_real_NT
@@ -51,7 +55,10 @@ Program test_distributed_matrix
   Call test_ks_matrix_matmul_complex_TN
   Call test_ks_matrix_matmul_complex_NT
   Call test_ks_matrix_matmul_complex_TT
-  
+
+  ! KS_array tests
+  Call test_ks_array_matmul_NN
+
   Call mpi_finalize( error )
 
 Contains
@@ -867,5 +874,122 @@ Contains
     Call ks_matrix_finalise
     
   End Subroutine test_ks_matrix_matmul_complex_TT
+
+  Subroutine test_ks_array_matmul_NN
+
+    Use numbers_module , Only : wp
+    Use ks_array_module, Only : ks_array, ks_array_init, ks_array_comm_to_base, ks_array_finalise, &
+         K_POINT_REAL, K_POINT_COMPLEX
+    Use mpi            , Only : mpi_bcast, mpi_comm_world, mpi_double_complex, mpi_double_precision, &
+         MPI_Logical
+
+    Real   ( wp ), Dimension( :, :, :, : ), Allocatable :: A_r, B_r, C_r
+    Complex( wp ), Dimension( :, :, :, : ), Allocatable :: A_c, B_c, C_c
+
+    Real   ( wp ), Dimension( :, : ), Allocatable :: tmp_r
+    Complex( wp ), Dimension( :, : ), Allocatable :: tmp_c
+
+    Real( wp ), Dimension( :, : ), Allocatable :: rand1, rand2
+
+    Real( wp ) :: max_diff
+    Real( wp ) :: rand
+
+    Type( ks_array ) :: base
+    Type( ks_array ) :: Am, Bm, Cm
+    
+    Integer, Dimension( 1:3, 1:nk ) :: k_points
+    Integer, Dimension(      1:nk ) :: k_types
+
+    Integer :: kpoint, spin
+
+    Allocate( A_r( 1:m, 1:k, 1:nk, 1:ns ) )
+    Allocate( B_r( 1:k, 1:n, 1:nk, 1:ns ) )
+    Allocate( C_r( 1:m, 1:n, 1:nk, 1:ns ) )
+    Allocate( A_c( 1:m, 1:k, 1:nk, 1:ns ) )
+    Allocate( B_c( 1:k, 1:n, 1:nk, 1:ns ) )
+    Allocate( C_c( 1:m, 1:n, 1:nk, 1:ns ) )
+    If( me == 0 ) Then
+       Do spin = 1, ns
+          Do kpoint = 1, nk
+             k_points( :, kpoint ) = [ kpoint - 1, 0, 0 ]
+             Call Random_number( rand )
+             k_types( kpoint ) = Merge( K_POINT_REAL, K_POINT_COMPLEX, rand > 0.5_wp )
+             If( k_types( kpoint ) == K_POINT_REAL ) Then
+                ! Real
+                Call Random_number( A_r( :, :, kpoint, spin ) )
+                Call Random_number( B_r( :, :, kpoint, spin ) )
+                C_r( :, :, kpoint, spin ) = &
+                     Matmul( A_r( :, :, kpoint, spin ), B_r( :, :, kpoint, spin ) )
+             Else
+                ! Complex
+                Allocate( rand1( 1:m, 1:k ), rand2( 1:m, 1:k ) )
+                Call Random_number( rand1 ); Call Random_number( rand2 ) 
+                A_c( :, :, kpoint, spin ) = Cmplx( rand1, rand2, wp )
+                Deallocate( rand1, rand2 )
+                Allocate( rand1( 1:k, 1:n ), rand2( 1:k, 1:n ) )
+                Call Random_number( rand1 ); Call Random_number( rand2 ) 
+                B_c( :, :, kpoint, spin ) = Cmplx( rand1, rand2, wp )
+                Deallocate( rand1, rand2 )
+                C_c( :, :, kpoint, spin ) = &
+                     Matmul( A_c( :, :, kpoint, spin ), B_c( :, :, kpoint, spin ) )
+             End If
+          End Do
+       End Do
+    End If
+
+    Call mpi_bcast( k_points, Size( k_points ), mpi_integer, 0, mpi_comm_world, error )
+    Call mpi_bcast( k_types , Size( k_types  ), mpi_logical, 0, mpi_comm_world, error )
+    
+    Call mpi_bcast( A_r, Size( A_r ), mpi_double_precision, 0, mpi_comm_world, error )
+    Call mpi_bcast( B_r, Size( B_r ), mpi_double_precision, 0, mpi_comm_world, error )
+    Call mpi_bcast( C_r, Size( C_r ), mpi_double_precision, 0, mpi_comm_world, error )
+
+    Call mpi_bcast( A_c, Size( A_c ), mpi_double_complex  , 0, mpi_comm_world, error )
+    Call mpi_bcast( B_c, Size( B_c ), mpi_double_complex  , 0, mpi_comm_world, error )
+    Call mpi_bcast( C_c, Size( C_c ), mpi_double_complex  , 0, mpi_comm_world, error )
+    
+    Allocate( tmp_r( 1:m, 1:n ) )
+    Allocate( tmp_c( 1:m, 1:n ) )
+
+    Call ks_array_init
+    Call ks_array_comm_to_base( MPI_COMM_WORLD, ns, k_types, k_points, base )
+
+    Call Am%create( m, k, base )
+    Call Bm%create( k, n, Am   )
+    Do spin = 1, ns
+       Do kpoint = 1, nk
+          If( k_types( kpoint ) == K_POINT_REAL ) Then
+             Call Am%set_by_global( spin, k_points( :, kpoint ), 1, m, 1, k, A_r( :, :, kpoint, spin ) )
+             Call Bm%set_by_global( spin, k_points( :, kpoint ), 1, k, 1, n, B_r( :, :, kpoint, spin ) )
+          Else
+             Call Am%set_by_global( spin, k_points( :, kpoint ), 1, m, 1, k, A_c( :, :, kpoint, spin ) )
+             Call Bm%set_by_global( spin, k_points( :, kpoint ), 1, k, 1, n, B_c( :, :, kpoint, spin ) )
+          End If
+       End Do
+    End Do
+
+    Cm = Am * Bm
+
+    Call cm%print_info( 'Cm - the result matrix', 9999 )
+    max_diff = -1.0_wp
+    Do spin = 1, ns
+       Do kpoint = 1, nk
+          If( k_types( kpoint ) == K_POINT_REAL ) Then
+             Call Cm%get_by_global( spin, k_points( :, kpoint ), 1, m, 1, n, tmp_r ) 
+             max_diff = Max( max_diff, Maxval( Abs( C_r( :, :, kpoint, spin ) - tmp_r ) ) )
+          Else
+             Call Cm%get_by_global( spin, k_points( :, kpoint ), 1, m, 1, n, tmp_c ) 
+             max_diff = Max( max_diff, Maxval( Abs( C_c( :, :, kpoint, spin ) - tmp_c ) ) )
+          End If
+       End Do
+    End Do
+    If( me == 0 ) Then
+       Write( *, format ) 'Error in ks_array matmul NN ', max_diff, &
+            Merge( 'Passed', 'FAILED', max_diff < tol )
+    End If
+
+    Call ks_array_finalise
+
+  End Subroutine test_ks_array_matmul_NN
 
 End Program test_distributed_matrix
